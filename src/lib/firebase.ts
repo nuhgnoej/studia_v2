51a1ed6968b3261bafcf74098d210f17049b5f7a
nuhgnoej <odineyes@naver.com>
2025-07-14 04:18:27 +0900

    getReactNativePersistence TS Error ignore

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,6 +3,7 @@
 import { initializeApp } from "firebase/app";
 // import { getAuth } from "firebase/auth";
 import AsyncStorage from '@react-native-async-storage/async-storage';
+// @ts-ignore
 import { getReactNativePersistence, initializeAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 // TODO: Add SDKs for Firebase products that you want to use
